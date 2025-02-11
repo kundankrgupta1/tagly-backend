83b15d6522f2b89876e1a41dd9dc9b83e2f9771d
Kundan Kumar Gupta <kundankrgupta@outlook.com>
2025-02-11 14:19:24 +0530

    update

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ import postRoutes from "../routes/post.route.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/health-check", (req, res) => res.status(200).send("Server is live..."));
 app.use(userRoutes)
 app.use(postRoutes)
 export default app;
